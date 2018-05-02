9532e6ac80b4234539bdeb55119d9cfa80f4282e
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-05-02 16:04:21 +0100

    Add example solution

--- src/main/scala/app/AuthAlg.scala
+++ src/main/scala/app/AuthAlg.scala
@@ -12,7 +12,33 @@ abstract class AuthAlg[F[_]](
   context: ExecutionContext,
   timer: Timer[F]
 ) {
-  final def requestNewAuthToken: F[AuthToken] = ???
+  final def requestNewAuthToken: F[AuthToken] =
+    timer.now.map(now => AuthToken(now.plusSeconds(3600), "token"))
 
-  final def requestActiveAuthToken: F[F[AuthToken]] = ???
+  final def requestActiveAuthToken: F[F[AuthToken]] =
+    for {
+      ref <- refOf[F, Option[AuthToken]](None)
+      queue <- unboundedQueue[F, Promise[F, AuthToken]]
+      fiber <- (for {
+        promise <- queue.dequeue1
+        existingAuthToken <- ref.get
+        now <- timer.now
+        authToken <- existingAuthToken
+          .filter(_.isActive(now))
+          .map(_.pure[F])
+          .getOrElse {
+            for {
+              newAuthToken <- requestNewAuthToken
+              _ <- ref.setSync(Some(newAuthToken))
+            } yield newAuthToken
+          }
+        _ <- promise.complete(authToken)
+      } yield ()).forever.start
+      activeAuthToken = for {
+        promise <- promise[F, AuthToken]
+        _ <- queue.offer1(promise)
+        authToken <- (fiber.join race promise.get)
+          .collect { case Right(authToken) => authToken }
+      } yield authToken
+    } yield activeAuthToken
 }
